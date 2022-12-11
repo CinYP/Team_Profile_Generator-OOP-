9b41d3e40527332b7426e64a3174dea4182e6038
CinYP <111353883+CinYP@users.noreply.github.com>
2022-12-11 15:45:07 -0500

    All test passing

--- tests_/Employee.test.js
+++ tests_/Employee.test.js
@@ -4,34 +4,34 @@ const Employee = require('../lib/Employee');
 describe("Employee", ()=>{
     describe("Initialization", ()=>{
         it("should return an object containing information about the manager", ()=>{
-        const employee = new Employee('Cinthia',00125,'cinthiapruitt@gmail.com'); 
+        const employee = new Employee('Cinthia','00125','cinthiapruitt@gmail.com'); 
         
             expect(employee.name).toEqual(expect.any(String));
-            expect(employee.id).toEqual(expect.any(Number));
+            expect(parseInt(employee.id)).toEqual(expect.any(Number));
             expect(employee.email).toEqual(expect.any(String));
 
         }); 
 
         it("should return employee name", ()=>{
-            const employee = new Employee('Cinthia',00125,'cinthiapruitt@gmail.com'); 
+            const employee = new Employee('Cinthia','00125','cinthiapruitt@gmail.com'); 
 
             expect(employee.getName()).toEqual(expect.any(String));
 
         });
         it("should return employee ID", ()=>{
-            const employee = new Employee('Cinthia',00125,'cinthiapruitt@gmail.com'); 
+            const employee = new Employee('Cinthia','00125','cinthiapruitt@gmail.com'); 
 
-            expect(employee.getId()).toEqual(expect.any(Number));
+            expect(parseInt(employee.getId())).toEqual(expect.any(Number));
 
         });
         it("should return employee email", ()=>{
-            const employee = new Employee('Cinthia',00125,'cinthiapruitt@gmail.com'); 
+            const employee = new Employee('Cinthia','00125','cinthiapruitt@gmail.com'); 
 
             expect(employee.getEmail()).toEqual(expect.any(String));
 
         });
         it("should return employee role", ()=>{
-            const employee = new Employee('Cinthia',00125,'cinthiapruitt@gmail.com'); 
+            const employee = new Employee('Cinthia','00125','cinthiapruitt@gmail.com'); 
 
             expect(employee.getRole()).toEqual('Employee');
 
